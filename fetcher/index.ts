d9c208453d06b46c3905a6eb23297c3fc36ed810
Gusti Alfian M. P <gustialfianmp@gmail.com>
2023-09-28 11:30:30 +0700

    fetcher ok

--- fetcher/index.ts
+++ fetcher/index.ts
@@ -1,78 +1,65 @@
 import * as Http from "@effect/platform-node/HttpClient";
-// import * as NodeClient from "@effect/platform-node/Http/NodeClient"
 import { ParseError } from "@effect/schema/ParseResult";
 import * as S from "@effect/schema/Schema";
 import { Console, Context, Duration, Effect, Layer, LogLevel, Logger, Schedule, pipe } from "effect";
 
+// domain
 const Todo = S.struct({
     userId: S.number,
-    id:  S.number,
+    id: S.number,
     title: S.string,
     completed: S.boolean,
 })
 type Todo = S.Schema.To<typeof Todo>
 
+const getTodo = (id: number) => Effect.gen(function* (_) {
+    yield* _(Effect.logDebug(`getTodo start: ${id}`))
+    const client = Http.client.fetch()
+    const req = Http.request.get(`https://jsonplaceholder.typicode.com/todos/${id}`)
 
-const retrySchedule = pipe(
+    const res = yield* _(client(req), Effect.retry(policy))
+    const result = yield* _(Http.response.schemaBodyJson(Todo)(res))
+
+    yield* _(Effect.logDebug(`getTodo done: ${id}`))
+    return result
+}).pipe(Logger.withMinimumLogLevel(LogLevel.Debug))
+
+const getTodos = (ids: number[]) => Effect.all(ids.map((x) => getTodo(x)), { concurrency: 2 })
+
+
+// retry policy
+const policy = pipe(
     Schedule.exponential(Duration.millis(10), 2),
     Schedule.either(Schedule.spaced(Duration.seconds(1))),
     Schedule.compose(Schedule.elapsed),
-    Schedule.whileOutput(Duration.lessThanOrEqualTo(Duration.seconds(30))),
+    Schedule.whileOutput(Duration.lessThanOrEqualTo(Duration.seconds(20))),
 )
 
+
+// service
 interface TodosService {
-  readonly getTodo: (id: number) => Effect.Effect<never, FetchError | JsonBodyError | ParseError, Todo>
-  readonly getTodos: (ids: number[]) => Effect.Effect<never, FetchError | JsonBodyError | ParseError, Todo[]>
+    readonly getTodo: (id: number) => Effect.Effect<never, ParseError | Http.error.HttpClientError, Todo>
+    readonly getTodos: (ids: number[]) => Effect.Effect<never, ParseError | Http.error.HttpClientError, Todo[]>
 }
 const TodosService = Context.Tag<TodosService>()
-
-const getTodo = (id: number) => pipe(
-    request(`https://jsonplaceholder.typicode.com/todos/${id}`),
-    Effect.tap(() => Effect.logDebug(`getTodo ${id} start...`)),
-    Effect.tap(() => Effect.sleep(Duration.millis(100))), // simulate waiting
-    Effect.flatMap(jsonBody),
-    Effect.flatMap(S.parse(Todo)),
-    Effect.retry(retrySchedule),
-    Effect.tap(() => Effect.logDebug(`getTodo ${id} done!`)),
-    Effect.provide(Logger.minimumLogLevel(LogLevel.Debug)),
-)
-
-const getTodo1 = (id: number) => Effect.gen(function* (_) {
-    const defaultClient = yield* _(Http.client.Client)
-    const todoClient = pipe(
-        defaultClient,
-        Http.client.mapRequest(Http.request.prependUrl("https://jsonplaceholder.typicode.com")),
-        Http.client.mapEffect(Http.response.schemaBodyJson(Todo))
-    )
-    const res = yield* _(Http.request.get(`/todos/${id}`), todoClient)
-    return res
-})
-
-const getTodos = (ids: number[]) =>
-    Effect.all(ids.map((x) => getTodo(x)), { concurrency: 2 })
-
 
 const TodosServiceLive = Layer.succeed(TodosService, TodosService.of({
     getTodo: getTodo,
     getTodos: getTodos,
 }))
 
+
+// main function
 const main: Effect.Effect<never, never, void> = pipe(
-    // Effect.all({ TodosService }),
-    // Effect.flatMap(({ TodosService: srv }) => srv.getTodos([1, 2, 3, 4, 5])),
-    getTodo1(1),
-    Effect.flatMap((x) => Console.log(`result:`, x)),
+    Effect.all({ TodosService }),
+    Effect.flatMap(({ TodosService: srv }) => srv.getTodos([1, 2, 3, 4, 5])),
+    Effect.flatMap((x) => Console.log(`result:`, x.map(v => `${v.id} ${v.title}`))),
     Effect.catchTags({
-        // FetchError: (err) => Console.error(err),
-        // JsonBodyError: (err) => Console.error(err),
         ParseError: (err) => Console.error(err),
         RequestError: (err) => Console.error(err),
         ResponseError: (err) => Console.error(err),
     }),
-    Effect.provide(
-        // TodosServiceLive,
-        Http.nodeClient.layer
-    ),
+    Effect.provide(TodosServiceLive),
 )
 
 Effect.runFork(main)
